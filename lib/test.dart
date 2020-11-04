b36dfc7af7fb91dfab592ecb7c018de843e96bcc
fikryans <fikriansyah.hq@gmail.com>
2020-11-04 16:25:16 +0700

    halo sendy

--- lib/test.dart
+++ lib/test.dart
@@ -11,3 +11,15 @@ class NewsApp extends StatelessWidget {
     return Container();
   }
 }
+
+class haloSendy extends StatefulWidget {
+  @override
+  _haloSendyState createState() => _haloSendyState();
+}
+
+class _haloSendyState extends State<haloSendy> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
