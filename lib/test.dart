f13ebef789962c6f66961856ad91f17a0506d5c2
fikryans <fikriansyah.hq@gmail.com>
2020-11-04 16:19:23 +0700

    commit tes.dart

--- lib/test.dart
+++ lib/test.dart
@@ -4,3 +4,10 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(NewsApp());
 }
+
+class NewsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
